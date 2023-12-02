83589a9f034be7347e33edaf23047df652a0c315
Ihuoma Steven <ihuomaadanne@gmail.com>
2023-12-02 12:25:08 +0100

    manually add getters and setters

--- event-consumer/src/main/java/evento/KafkaProperty.java
+++ event-consumer/src/main/java/evento/KafkaProperty.java
@@ -1,14 +1,36 @@
 package evento;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Data
 @Configuration
 @ConfigurationProperties("spring.kafka.consumer")
 public class KafkaProperty {
     private String bootstrapServers;
     private String groupId;
     private String clientId;
+
+    public String getBootstrapServers() {
+        return bootstrapServers;
+    }
+
+    public void setBootstrapServers(String bootstrapServers) {
+        this.bootstrapServers = bootstrapServers;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
 }

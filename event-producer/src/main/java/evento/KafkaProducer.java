8941a649f61583928eef63a822d637782ba59dbc
Ihuoma Steven <ihuomaadanne@gmail.com>
2023-12-02 07:19:59 +0100

    use a generic object

--- event-producer/src/main/java/evento/KafkaProducer.java
+++ event-producer/src/main/java/evento/KafkaProducer.java
@@ -1,24 +1,37 @@
 package evento;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class KafkaProducer {
 
-    private final KafkaTemplate<String, User> kafkaTemplate;
+    private final KafkaTemplate<String, Object> template;
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
 
-    public KafkaProducer(KafkaTemplate<String, User> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    public KafkaProducer(KafkaTemplate<String, Object> template) {
+        this.template = template;
     }
 
     public void send(User user) {
-        kafkaTemplate.send(KafkaConstants.USER_TOPIC, user);
-//        var message = MessageBuilder
-//                .withPayload(user)
-//                .setHeader(KafkaHeaders.TOPIC, KafkaConstants.USER_TOPIC)
-//                .build();
-//        kafkaTemplate.send(message);
+        try {
+            CompletableFuture<SendResult<String, Object>> future = template.send(KafkaConstants.USER_TOPIC, user);
+            future.whenComplete((result, ex) -> {
+                if (ex == null) {
+                    log.info("Sent message=[{}] with offset=[{}]", user.toString(), result.getRecordMetadata().offset());
+                } else {
+                    log.error("Unable to send message=[{}] due to: {}", user.toString(), ex.getMessage());
+                }
+            });
+
+        } catch (Exception ex) {
+            log.error("ERROR: {}", ex.getMessage());
+        }
     }
 
 }

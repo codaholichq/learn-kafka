b90255de1f70e302a189c4d7a1dbfb8a85646f1f
Ihuoma Steven <ihuomaadanne@gmail.com>
2023-12-02 07:06:10 +0100

    remove containerFactory
    remove containerFactory since we are using standard factory name - kafkaListenerContainerFactory

--- event-consumer/src/main/java/evento/KafkaConsumer.java
+++ event-consumer/src/main/java/evento/KafkaConsumer.java
@@ -1,11 +1,12 @@
 package evento;
 
+import evento.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class KafkaConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
@@ -15,10 +16,7 @@ public class KafkaConsumer {
         return receivedUser;
     }
 
-    @KafkaListener(
-            topics = KafkaConstants.USER_TOPIC,
-            groupId = KafkaConstants.GROUP_ID,
-            containerFactory = "userListener")
+    @KafkaListener(topics = KafkaConstants.USER_TOPIC, groupId = KafkaConstants.GROUP_ID)
     public void fetch(User user) {
         log.info("Received message => {}", user.toString());
         this.receivedUser = user;

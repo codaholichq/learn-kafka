c90c27d7b3829c56c25a017affbe7446b4000396
Ihuoma Steven <ihuomaadanne@gmail.com>
2023-12-02 07:06:58 +0100

    optmise imports

--- event-consumer/src/main/java/evento/UserController.java
+++ event-consumer/src/main/java/evento/UserController.java
@@ -1,5 +1,6 @@
 package evento;
 
+import evento.model.User;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
